edca1a132f434cabe46692a3e6644072fbcf5ef3
suchipatel13 <127153219+suchipatel13@users.noreply.github.com>
2023-03-24 18:33:12 -0400

    Create lab6.c

---
+++ lab6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void calculate (int x, int y, int*sum, int*difference, int*product);
+
+int main(int argc,char** argv){
+        int x,y,sum,difference,product;
+        printf("Enter two integers separated by a space\n");
+        scanf("%d %d",&x,&y);
+        calculate(x,y,&sum,&difference,&product);
+        printf("The sum of %d and %d is %d\n",x,y,sum);
+        printf("The difference of %d and %d is %d\n",x,y,difference);
+        printf("The product of %d and %d is %d\n",x,y,product);
+}
+
+void calculate (int x, int y, int*sum, int*difference, int*product){
+*sum = x + y;
+*product = x * y;
+*difference = x - y;
+}
